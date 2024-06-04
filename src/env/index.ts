3107fb2e5bef36c74e6c13816c03a03332aeab1c
Murilo Bagodi <murilosbagodi@hotmail.com>
2024-06-04 15:27:35 -0300

    fix env 0.1

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,9 +1,9 @@
-import { config }from "dotenv"
+import { config } from "dotenv"
 import { z } from "zod"
 
-if(process.env.NODE_ENV ==="test"){
-    config({path: ".env.test"})
-}else{
+if (process.env.NODE_ENV === "test") {
+    config({ path: ".env.test" })
+} else {
     config()
 }
 
@@ -11,12 +11,12 @@ const envSchema = z.object({
     NODE_ENV: z.enum(['development', 'test', 'production']).default("production"),
     DATABASE_CLIENT: z.enum(["sqlite", "pg"]),
     DATABASEURL: z.string(),
-    PORT:  z.coerce.number().default(3333)
+    PORT: z.coerce.number().default(3333)
 })
 
 const _env = envSchema.safeParse(process.env)
 
-if(_env.success == false){
+if (_env.success == false) {
     console.error("invalid enviroment variable: ", _env.error.format())
 
     throw new Error("Invalid Enviroment variable")
