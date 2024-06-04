e065a9b804c917fa734a645797507407ff217a22
Murilo Bagodi <murilosbagodi@hotmail.com>
2024-06-04 15:40:59 -0300

    fix env 0.2

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import { app } from "./app"
 import { env } from "./env"
+const port = env.PORT  || 3333
 
 try {
-    app.listen({ port: env.PORT }).then(() => {
-        console.log("HTTP Server Running")
+    app.listen({ port }).then(() => {
+        console.log("HTTP Server Running on: ", port)
     })
 } catch {
     console.log("HTTP Server is not Running")
